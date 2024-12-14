c2ebc32549520620b633a824eaef56016c6ac239
Samilly <samillynunes19@gmail.com>
2024-12-14 14:10:56 -0300

    feat: fetching markets

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,12 +1,18 @@
 import { Alert, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 import { api } from "@/services/api";
-import { useEffect, useState } from "react";
 import { CategoriesList, CategoriesProps } from "@/components/categories_list";
+import { PlaceProps } from "@/components/place";
+
+type MarketsProps = PlaceProps & {
+    
+}
 
 export default function Home(){
     const [categories, setCategories] = useState<CategoriesProps>([]);
     const [categorySelected, setCategorySelected] = useState("");
+    const [markets, setMarkets] = useState<MarketsProps[]>([]);
 
     async function fetchCategories(){
         try {
@@ -19,9 +25,26 @@ export default function Home(){
         }
     }
 
+    async function fetchMarkets(){
+        try {
+            if(!categorySelected) return;
+            const response = await api.get("/markets/category/"+categorySelected);
+            setMarkets(response.data);
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Locais', 'Não foi possívei carregar os locais');
+        }
+    }
+
     useEffect(()=>{
         fetchCategories();
     }, []);
+
+    // Esse hook vai ser dependente da mudanca da categoria selecionada, ou seja,
+    // quando mudar a categoria, vai buscar novamente os locais/estabelecimentos
+    useEffect(()=> {
+        fetchMarkets();
+    },[categorySelected]);
 
     return (
         <View style={{flex: 1}}>
---
+++ src/components/places_list/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+
+import { Place, PlaceProps } from "../place";
+import { s } from "./style";
+
+type Props = {
+    data: PlaceProps[],
+}
+
+export function PlacesList({ data }: Props){
+    return <View>
+        
+    </View>
+}
